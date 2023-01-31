68177682190a01cefecf07e4ab821daa5f1e8484
Brian Bird <birdb@lanecc.edu>
2023-01-31 10:58:14 -0800

    Adding UserController in progress.

---
+++ BookReviews/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using BookReviews.Models;
+using System.Data;
+
+namespace BookReviews.Controllers
+{
+   // [Authorize(Role = "Admin")]
+    public class UserController : Controller
+    {
+        private UserManager<AppUser> userManager; private RoleManager<IdentityRole> roleManager;
+        public UserController(UserManager<AppUser> userMngr, RoleManager<IdentityRole> roleMngr)
+        {
+            userManager = userMngr;
+            roleManager = roleMngr;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<AppUser> users = new List<AppUser>(); 
+            foreach (AppUser user in userManager.Users)
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user); 
+                users.Add(user);
+            }
+            UserViewModel model = new UserViewModel
+            {
+                Users = users,
+                Roles = roleManager.Roles
+            }; return View(model);
+        } // the other action methods }
+    }
+}
