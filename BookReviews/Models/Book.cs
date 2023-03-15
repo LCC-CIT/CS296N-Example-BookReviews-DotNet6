0fce1c9ea3924daae8bb2d2b9d9fba7f510e9a19
Brian Bird <birdb@lanecc.edu>
2023-03-14 18:47:39 -0700

    Changes from notebook

--- BookReviews/Models/Book.cs
+++ BookReviews/Models/Book.cs
@@ -16,10 +16,14 @@ public class Book
     public ulong Isbn { get; set; }
 
     public string? Publisher { get; set; }
+<<<<<<< Updated upstream
 
     // [Range(typeof(DateTime), "1/1/1753", "12/31/2100", ErrorMessage = "Date for {0} must be between {1} and {2}")]
+=======
+    // [Range(typeof(DateTime), "01/01/1450", "12/31/2099", ErrorMessage = "Date for {0} must be between {1} and {2}")]
+>>>>>>> Stashed changes
     [DisplayFormat(DataFormatString = "{0:MMM d, yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime PubDate { get; set; }
+     public DateTime PubDate { get; set; }
 
     public List<Author> Authors { get; } = new();
 
